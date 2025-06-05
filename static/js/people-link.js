170fad07f5d2426e3eadbcc2895651f273bb07b8
sigma-ouc <sigma.ouc@gmail.com>
2025-06-05 04:04:36 +0000

    update

--- static/js/people-link.js
+++ static/js/people-link.js
@@ -1,30 +1,31 @@
 document.addEventListener('DOMContentLoaded', function() {
     // 定义每个用户的姓名和对应的外部链接
     const userLinks = {
+        'Yanwei Yu': 'https://yuyanwei.github.io/index.html',
+        'Jianpeng Qi': 'https://qijianpeng.github.io/',
         'Dongliang Chen': 'https://cdl1102.github.io/cdl.github.io/',
+        'Bosen Lin': 'https://catchacat083.github.io/',
         'Xiang Li': 'https://lx970414.github.io/',
         'Xingyu Zhao': 'https://lion1a.github.io/',
-        'Jianpeng Qi': 'https://qijianpeng.github.io/',
-        'Bosen Lin': 'https://catchacat083.github.io/',
         'Kunyu Ni': 'https://kunyuni.github.io/',
-        'Chenlong Song': 'https://songchenlong.github.io/'
+        'Chenlong Song': 'https://songchenlong.github.io/',
         // 可根据需要添加更多用户信息
     };
 
-    // 选择所有包含用户姓名的 <a> 标签
-    const personNames = document.querySelectorAll('a');
+    // 选择所有头像和姓名的 <a> 标签
+    const personLinks = document.querySelectorAll('.people-person a');
 
-    personNames.forEach(function(nameElement) {
-        const personName = nameElement.textContent.trim();
+    personLinks.forEach(function(linkElement) {
+        const personName = linkElement.textContent.trim();
         const externalLink = userLinks[personName];
 
         if (externalLink) {
             // 如果有外部链接，阻止默认跳转，跳转到外部链接
-            nameElement.addEventListener('click', function(event) {
+            linkElement.addEventListener('click', function(event) {
                 event.preventDefault();
                 window.open(externalLink, '_blank');
             });
-            nameElement.style.cursor = 'pointer'; // 鼠标悬停时显示指针样式
+            linkElement.style.cursor = 'pointer'; // 鼠标悬停时显示指针样式
         }
         // 如果没有外部链接，保持默认的跳转行为
     });
